d9c765806ee058a587726e4d520df1f43ed413d5
jeremiaslmf <jeremiaslmf@gmail.com>
2019-04-27 14:31:14 -0300

    Criacao classe ProdutoService

--- src/main/java/br/edu/unicesumar/rest/ProdutoService.java
+++ src/main/java/br/edu/unicesumar/rest/ProdutoService.java
@@ -1,5 +1,7 @@
 package br.edu.unicesumar.rest;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -7,11 +9,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import br.edu.unicesumar.dao.ProdutoDAO;
 import br.edu.unicesumar.model.Produto;
 
 @Path("/json/produto")
 public class ProdutoService {
 
+	private ProdutoDAO dao = new ProdutoDAO();
+	
 	@GET
 	@Path("/get")
 	@Produces("application/json")
@@ -20,7 +25,7 @@ public class ProdutoService {
 		Produto produto = new Produto();
 		produto.setNome("Moto X - Android");
 		produto.setQuantidade(10);
-
+	
 		return produto;
 
 	}
@@ -30,7 +35,23 @@ public class ProdutoService {
 	@Consumes("application/json")
 	public Response criarProdutoJSON(Produto produto) {
 
-		String result = "Product criado : " + produto;
+		dao.save(produto);
+		
+		String result = "produto Criado : " + produto.toString();
 		return Response.status(201).entity(result).build();
+
 	}
+	
+	
+	@GET
+	@Path("/listas")
+	@Produces("application/json")
+	public Response listar() {
+
+		List<Produto> produtos = dao.list();
+		String resultado = "quantidade de produtos: "+ produtos.size();
+		return Response.status(201).entity(resultado).build();
+
+	}
+
 }
